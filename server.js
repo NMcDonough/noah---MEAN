2a8566174021916fcf78e958d86eb8c4a03ab8c6
nmcdonough <mcnoah94@gmail.com>
2018-08-11 20:45:13 -0400

    removed glyphs from git

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const mongoose = require('mongoose')
-const path = require('path')
-const flash = require('express-flash')
-
-mongoose.Promise = global.Promise
-
-app.use(flash())//  Use flash
-app.use(bodyParser.urlencoded({encoded: true}))// Use body parser
-app.use(express.static(path.join(__dirname, './client/static')))// Identify static folder
-app.use(session({secret: 'SECRET_KEY'}))
-app.set('views', path.join(__dirname, "./public/src"))// Identify views folders
-app.set('view engine', 'ejs')// Set EJS as the view engine
-
-require('./server/config/routes.js')(app)
-require('./server/config/mongoose.js')
-
-app.listen(8000, function(){
-    console.log('Listening on port 8000')
+var express = require("express");
+var path = require("path");
+const app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, './public/src/assets')));
+app.set('views', "./public/src"); 
+app.set('view engine', 'ejs');
+app.all("*", (req,res,next) => {
+    res.sendFile(path.resolve('public/dist/public/index.html'))
 })
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+});
--- server/config/routes.js
+++
@@ -1,8 +0,0 @@
-const controller = require('../controllers/controller.js')//    'controller' can be whatever you chose to use for the name of controller.js if you want
-const path = require("path");
-
-module.exports = function(app) {
-    app.all("*", (req,res,next) => {
-        res.sendFile(path.resolve('./public/dist/public/index.html'))
-    })
-}
